18c4dd0163a484708ac7624fd812f5b4e7d95306
Dharmik Rokad <130790665+dharmikRokad@users.noreply.github.com>
2024-04-04 17:29:59 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,25 @@
+// Conversion to a Number
+// let age = {}
+// let numberedValue = Number(age)
+// console.log(typeof numberedValue)
+// console.log(numberedValue);
+
+// "33" => 33 
+// "33ac" => NaN
+// true => 1
+// null => 0
+// undefined => NaN
+// onject => NaN
+
+// Conversion to a Boolean
+let isAbove = 0
+let booledValue = Boolean(isAbove)
+console.log(typeof booledValue)
+console.log(booledValue);
+
+// "filled string" => true
+// "empty string" => false
+// 0 => false; 1 => true
+// 123 => 
+// null => 
+//undefined => 
